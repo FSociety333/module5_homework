d9e9aa549dac2559d5e7e6fe83d3697a1c02295f
FSociety333 <drift93rus@gmail.com>
2022-11-13 17:23:45 +0300

    v2

--- task1.js
+++ task1.js
@@ -1,7 +1,8 @@
 let userNum = +prompt('Число не равное нулю:')
 if (isNaN(userNum) || userNum === 0 || typeof (userNum) !== 'number') {
   console.log('Ошибка')
-} else {
+} 
+else {
   let numVariant = userNum % 2 === 0 ? 'чет' : 'нечет'
   console.log(`${userNum} является ${numVariant}ным`)
 }
